194a24f0ef4e94dc2ded4e7fb197acb8704b08fb
bright <limingbright@yahoo.com>
2016-08-18 15:35:58 +0800

    proxy

--- src/main/java/com/ehualu/proxy/constraint/GamePlayerProxy.java
+++ src/main/java/com/ehualu/proxy/constraint/GamePlayerProxy.java
@@ -3,7 +3,7 @@ package com.ehualu.proxy.constraint;
 /**
  * Created by bright on 16-8-18.
  */
-public class GamePlayerProxy implements IGamePlayer{
+public class GamePlayerProxy implements IGamePlayer,IProxy{
 
     private IGamePlayer gameplayer = null;
 
@@ -21,9 +21,14 @@ public class GamePlayerProxy implements IGamePlayer{
 
     public void upgrade() {
         this.gameplayer.upgrade();
+        this.count();
     }
 
     public IGamePlayer getProxy() {
         return this;
+    }
+
+    public void count() {
+        System.out.println("升级的总费用是：150元");
     }
 }
---
+++ src/main/java/com/ehualu/proxy/constraint/IProxy.java
@@ -0,0 +1,8 @@
+package com.ehualu.proxy.constraint;
+
+/**
+ * Created by bright on 16-8-18.
+ */
+public interface IProxy {
+    public void count();
+}
